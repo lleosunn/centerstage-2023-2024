587e2ffb03feb31b9f3733fa44ed4f1230781bb8
rachit-j <rjaiswal.sd.77@gmail.com>
2023-11-11 16:24:21 -0800

    Added detection stuff

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/objectdetection/br.java
@@ -0,0 +1,438 @@
+package org.firstinspires.ftc.teamcode.objectdetection;
+
+import com.arcrobotics.ftclib.controller.PIDController;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.teamcode.RobotHardware;
+import org.firstinspires.ftc.teamcode.odometry;
+import org.opencv.core.Scalar;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.opencv.core.Scalar;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+
+@Autonomous
+public class br extends LinearOpMode {
+
+    private PIDController movePID;
+    public static double p = 0.15, i = 0.5, d = 0.00000001; //0.15, 0.5, 8 0s 8
+
+    private OpenCvCamera webcam;
+
+    private static final int CAMERA_WIDTH  = 640; // width  of wanted camera resolution
+    private static final int CAMERA_HEIGHT = 360; // height of wanted camera resolution
+
+    private double CrLowerUpdate = 160;
+    private double CbLowerUpdate = 100;
+    private double CrUpperUpdate = 255;
+    private double CbUpperUpdate = 255;
+
+    public static double borderLeftX    = 0.0;   //fraction of pixels from the left side of the cam to skip
+    public static double borderRightX   = 0.0;   //fraction of pixels from the right of the cam to skip
+    public static double borderTopY     = 0.0;   //fraction of pixels from the top of the cam to skip
+    public static double borderBottomY  = 0.0;   //fraction of pixels from the bottom of the cam to skip
+
+    private double lowerruntime = 0;
+    private double upperruntime = 0;
+
+    // Red Range                                      Y      Cr     Cb
+    public static Scalar scalarLowerYCrCb = new Scalar(  0.0, 160.0, 100.0);
+    public static Scalar scalarUpperYCrCb = new Scalar(255.0, 255.0, 255.0);
+
+    // Yellow Range
+//    public static Scalar scalarLowerYCrCb = new Scalar(0.0, 100.0, 0.0);
+//    public static Scalar scalarUpperYCrCb = new Scalar(255.0, 170.0, 120.0);
+    static final double FEET_PER_METER = 3.28084;
+
+    // Lens intrinsics
+    // UNITS ARE PIXELS
+    // NOTE: this calibration is for the C920 webcam at 800x448.
+    // You will need to do your own calibration for other configurations!
+    double fx = 578.272;
+    double fy = 578.272;
+    double cx = 402.145;
+    double cy = 221.506;
+
+    // UNITS ARE METERS
+    double tagsize = 0.508; //Double check!!
+
+    AprilTagDetection tagOfInterest = null;
+
+    private static double maxpowermove = 0.95;
+    private static double maxpowerstay = 0.6;
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor fl = null;
+    private DcMotor fr = null;
+    private DcMotor bl = null;
+    private DcMotor br = null;
+    private DcMotor lift1 = null;
+    private DcMotor lift2 = null;
+    private DcMotor arm = null;
+
+    // Set where we want the robot to go
+    public boolean LEFT_DETECT = false;
+    public boolean MIDDLE_DETECT = false;
+    public boolean RIGHT_DETECT = false;
+
+    Servo claw;
+    Servo wrist;
+    Servo guider;
+
+    DcMotor verticalLeft, verticalRight, horizontal;
+    BNO055IMU imu;
+    BNO055IMU.Parameters parameters;
+    Orientation lastAngles = new Orientation();
+    double globalAngle;
+
+    public void imuinit() {
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        parameters = new BNO055IMU.Parameters();
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.loggingEnabled = false;
+        imu.initialize(parameters);
+
+        telemetry.addData("Gyro Mode", "calibrating...");
+        telemetry.update();
+
+        while (!isStopRequested() && !imu.isGyroCalibrated()) {
+            sleep(50);
+            idle();
+        }
+        telemetry.addData("Gyro Mode", "ready");
+        telemetry.addData("imu calib status", imu.getCalibrationStatus().toString());
+        telemetry.update();
+    }
+
+    private void resetAngle() {
+        lastAngles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        globalAngle = 0;
+    }
+
+    private double getAngle() {
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
+        if (deltaAngle < -180)
+            deltaAngle += 360;
+        else if (deltaAngle > 180)
+            deltaAngle -= 360;
+        globalAngle += deltaAngle;
+        lastAngles = angles;
+        return globalAngle;
+    }
+
+    final double COUNTS_PER_INCH = 1860;
+
+    odometry update;
+
+    @Override
+    public void runOpMode() {
+
+        movePID = new PIDController(p, i, d);
+
+        fl = hardwareMap.get(DcMotor.class, "fl");
+        fr = hardwareMap.get(DcMotor.class, "fr");
+        bl = hardwareMap.get(DcMotor.class, "bl");
+        br = hardwareMap.get(DcMotor.class, "br");
+        lift1 = hardwareMap.get(DcMotor.class, "lift1");
+        lift2 = hardwareMap.get(DcMotor.class, "lift2");
+        arm = hardwareMap.get(DcMotor.class, "arm");
+
+        claw = hardwareMap.get(Servo.class, "claw");
+        wrist = hardwareMap.get(Servo.class, "wrist");
+        guider = hardwareMap.get(Servo.class, "guider");
+
+        //odometers
+        verticalLeft = hardwareMap.dcMotor.get("fl");
+        verticalRight = hardwareMap.dcMotor.get("br");
+        horizontal = hardwareMap.dcMotor.get("fr");
+
+        RobotHardware robot = new RobotHardware(fl, fr, bl, br, lift1, lift2, arm, claw, wrist, guider);
+        robot.innitHardwareMap();
+
+        imuinit();
+        sleep(500);
+        telemetry.addData(">", "Press Play to start op mode");
+        telemetry.addData("angle", getAngle());
+        telemetry.update();
+
+        //start of camera code
+        // OpenCV webcam
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        //OpenCV Pipeline
+        ContourPipeline myPipeline;
+        webcam.setPipeline(myPipeline = new ContourPipeline(borderLeftX,borderRightX,borderTopY,borderBottomY));
+        // Configuration of Pipeline
+        myPipeline.configureScalarLower(scalarLowerYCrCb.val[0],scalarLowerYCrCb.val[1],scalarLowerYCrCb.val[2]);
+        myPipeline.configureScalarUpper(scalarUpperYCrCb.val[0],scalarUpperYCrCb.val[1],scalarUpperYCrCb.val[2]);
+        // Webcam Streaming
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                webcam.startStreaming(CAMERA_WIDTH, CAMERA_HEIGHT, OpenCvCameraRotation.UPSIDE_DOWN);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+
+
+        telemetry.update();
+
+
+        robot.clawClose();
+
+        waitForStart();
+        arm.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lift1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        lift2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //start odometry thread
+        update = new odometry(verticalLeft, verticalRight, horizontal, 10, imu);
+        Thread positionThread = new Thread(update);
+        positionThread.start();
+
+        int[] armHeight = {240, 160, 80, 0, 0};
+        double[] drift = {0.2, 0.4, 0.6, 0.8, 1};
+        resetRuntime();
+
+
+
+        //start of auto
+        while(opModeIsActive()){
+
+            // Camera Stuff
+            myPipeline.configureBorders(borderLeftX, borderRightX, borderTopY, borderBottomY);
+            if(myPipeline.error){
+                telemetry.addData("Exception: ", myPipeline.debug);
+            }
+            // Only use this line of the code when you want to find the lower and upper values
+            testing(myPipeline);
+
+            telemetry.addData("RectArea: ", myPipeline.getRectArea());
+            telemetry.update();
+
+            if(myPipeline.getRectArea() > 2000){
+                double rectMidpointX = myPipeline.getRectMidpointX();
+                double screenThird = CAMERA_WIDTH / 3.0;
+
+                if(rectMidpointX > 2 * screenThird){
+                    telemetry.addLine("OBJECT IS ON THE RIGHT SIDE");
+                }
+                else if(rectMidpointX > screenThird){
+                    telemetry.addLine("OBJECT IS IN THE MIDDLE");
+                }
+                else {
+                    telemetry.addLine("OBJECT IS ON THE LEFT SIDE");
+                    AUTONOMOUS_A();
+                }
+            }
+
+        }
+
+
+
+    }
+
+    public void moveTo(double targetX, double targetY, double targetOrientation, double error) {
+        double distanceX = targetX - (update.x() / COUNTS_PER_INCH);
+        double distanceY = targetY - (update.y() / COUNTS_PER_INCH);
+        double distance = Math.hypot(distanceX, distanceY);
+        while(opModeIsActive() && distance > error) {
+            distance = Math.hypot(distanceX, distanceY);
+            distanceX = targetX - (update.x() / COUNTS_PER_INCH);
+            distanceY = targetY - (update.y() / COUNTS_PER_INCH);
+
+            movePID.setPID(p, i, d);
+            double currentX = update.x() / COUNTS_PER_INCH;
+            double currentY = update.y() / COUNTS_PER_INCH;
+            double x = movePID.calculate(currentX, targetX);
+            double y = movePID.calculate(currentY, targetY);
+
+            double turn = 0.035 * (update.h() - targetOrientation);
+            double theta = Math.toRadians(update.h());
+            if (Math.abs(distanceX) < 1 || Math.abs(distanceY) < 1) {
+                movePID.reset();
+            }
+            if (x > maxpowermove) {
+                x = maxpowermove;
+            }
+            else if (x < -maxpowermove) {
+                x = -maxpowermove;
+            }
+            else x = x;
+            if (y > maxpowermove) {
+                y = maxpowermove;
+            }
+            else if (y < -maxpowermove) {
+                y = -maxpowermove;
+            }
+            else y = y;
+            if (turn > 0.3) {
+                turn = 0.3;
+            }
+            else if (turn < -0.3) {
+                turn = -0.3;
+            }
+            else turn = turn;
+            double l = y * Math.sin(theta + (Math.PI/4)) - x * Math.sin(theta - (Math.PI/4));
+            double r = y * Math.cos(theta + (Math.PI/4)) - x * Math.cos(theta - (Math.PI/4));
+            fl.setPower(l + turn);
+            fr.setPower(r - turn);
+            bl.setPower(r + turn);
+            br.setPower(l - turn);
+            if(isStopRequested()) {
+                update.stop();
+            }
+        }
+    }
+    public void stay(double targetX, double targetY, double targetOrientation) {
+        double distanceX = targetX - (update.x() / COUNTS_PER_INCH);
+        double distanceY = targetY - (update.y() / COUNTS_PER_INCH);
+        double x = 0.1 * distanceX;
+        double y = 0.1 * distanceY;
+        double turn = 0.035 * (update.h() - targetOrientation);
+        double theta = Math.toRadians(update.h());
+
+        if (x > maxpowerstay) {
+            x = maxpowerstay;
+        }
+        else if (x < -maxpowerstay) {
+            x = -maxpowerstay;
+        }
+        else x = x;
+        if (y > maxpowerstay) {
+            y = maxpowerstay;
+        }
+        else if (y < -maxpowerstay) {
+            y = -maxpowerstay;
+        }
+        else y = y;
+        if (turn > 0.3) {
+            turn = 0.3;
+        }
+        else if (turn < -0.3) {
+            turn = -0.3;
+        }
+        else turn = turn;
+
+        double l = y * Math.sin(theta + (Math.PI/4)) - x * Math.sin(theta - (Math.PI/4));
+        double r = y * Math.cos(theta + (Math.PI/4)) - x * Math.cos(theta - (Math.PI/4));
+
+        fl.setPower(l + turn);
+        fr.setPower(r - turn);
+        bl.setPower(r + turn);
+        br.setPower(l - turn);
+
+        if(isStopRequested()) {
+            update.stop();
+        }
+    }
+    public void testing(ContourPipeline myPipeline){
+        if(lowerruntime + 0.05 < getRuntime()){
+            CrLowerUpdate += -gamepad1.left_stick_y;
+            CbLowerUpdate += gamepad1.left_stick_x;
+            lowerruntime = getRuntime();
+        }
+        if(upperruntime + 0.05 < getRuntime()){
+            CrUpperUpdate += -gamepad1.right_stick_y;
+            CbUpperUpdate += gamepad1.right_stick_x;
+            upperruntime = getRuntime();
+        }
+
+        CrLowerUpdate = inValues(CrLowerUpdate, 0, 255);
+        CrUpperUpdate = inValues(CrUpperUpdate, 0, 255);
+        CbLowerUpdate = inValues(CbLowerUpdate, 0, 255);
+        CbUpperUpdate = inValues(CbUpperUpdate, 0, 255);
+
+        myPipeline.configureScalarLower(0.0, CrLowerUpdate, CbLowerUpdate);
+        myPipeline.configureScalarUpper(255.0, CrUpperUpdate, CbUpperUpdate);
+
+        telemetry.addData("lowerCr ", (int)CrLowerUpdate);
+        telemetry.addData("lowerCb ", (int)CbLowerUpdate);
+        telemetry.addData("UpperCr ", (int)CrUpperUpdate);
+        telemetry.addData("UpperCb ", (int)CbUpperUpdate);
+    }
+    public Double inValues(double value, double min, double max){
+        if(value < min){ value = min; }
+        if(value > max){ value = max; }
+        return value;
+    }
+    public void AUTONOMOUS_A(){
+        telemetry.addLine("Autonomous A");
+
+        // Manual reverse
+        double reversetime1 = 0.8 * 1000;
+        fl.setPower(-0.5);
+        fr.setPower(-0.5);
+        bl.setPower(-0.5);
+        br.setPower(-0.5);
+        sleep((long) reversetime1);
+        fl.setPower(0);
+        fr.setPower(0);
+        bl.setPower(0);
+        br.setPower(0);
+
+        // Manual turn
+        double manualturn1 = 0.5 * 1000;
+        fl.setPower(-0.5);
+        fr.setPower(0.5);
+        bl.setPower(0.5);
+        br.setPower(-0.5);
+        sleep((long) manualturn1);
+        fl.setPower(0);
+        fr.setPower(0);
+        bl.setPower(0);
+        br.setPower(0);
+
+        moveTo(30, 30, 0, 45);
+        sleep(30000);
+
+
+    }
+    public void AUTONOMOUS_B(){
+        telemetry.addLine("Autonomous B");
+        sleep(20);
+        MIDDLE_DETECT = true;
+
+    }
+    public void AUTONOMOUS_C(){
+        telemetry.addLine("Autonomous C");
+        sleep(20);
+        RIGHT_DETECT = true;
+
+    }
+}
+
+
+
